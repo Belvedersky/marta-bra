b3d8176aa21dc2c5337c2668344d29882db4c9dc
Belvedersky <qwink186@icloud.com>
2019-11-17 00:22:51 +0300

    yep

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const data = require("./public/data/data.json");
+const data = require("./public/data/catalog/catalog.json");
 var fs = require("fs");
 var cyrillicToTranslit = require("cyrillic-to-translit-js");
 
