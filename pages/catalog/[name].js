2311cc5785e4c510303cf1583cadd40ef7648009
Belvedersky <qwink186@icloud.com>
2019-11-17 19:59:16 +0300

    add cookies 🍪🍪🍪

--- pages/catalog/[name].js
+++ pages/catalog/[name].js
@@ -1,11 +1,11 @@
 import Body from "../../components/Body";
 import Content from "../../components/Content";
 import Image from "../../components/Images";
-
+import Cookies from 'js-cookie'
 import React, { Component } from 'react';
 
 class Index extends Component {
-  
+   
   static async getInitialProps({ query }) {
     const path  = query.name;
     console.log(path)
@@ -15,8 +15,23 @@ class Index extends Component {
   // let image = "/" + item.image.split("/").splice(2, 3).join("/");
 
   render(){
+
   if (!this.props.catalogItem) return <div>not found</div>;
-  const data = this.props.catalogItem.default.attributes
+
+   
+  const data = this.props.catalogItem.default.attributes;
+  const addItem =()=>{
+    let Order =[]
+    if(Cookies.get('Order')){
+      console.log(Cookies.get('Order'))
+      //console.log(Cookies.get('Order'));
+      //Order = [...Cookies.get('Order')];
+      //console.log(Order)
+    }
+    
+    Cookies.set('Order', JSON.stringify(Order.push(data)));
+
+  }
     // const image = "/" + this.props.item.image.split("/").splice(2, 3).join("/");
   return(
   <Body>
@@ -26,7 +41,9 @@ class Index extends Component {
         <h1> {data.title}</h1>
         <p>{data.description} </p>
         <p> ЦЕНА: {data.price}</p>
+        <button onClick={addItem}> Add</button>
       </div>
+      
     </Content>
   </Body>
   )
